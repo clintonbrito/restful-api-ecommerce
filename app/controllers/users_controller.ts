f44a04a43b817fc3f8417766a71bbf3b3b3fa834
Clinton Brito <clintoncsb@gmail.com>
2024-07-05 20:59:43 -0300

    🚧 wip: creating `UsersController`

---
+++ app/controllers/users_controller.ts
@@ -0,0 +1,35 @@
+import type { HttpContext } from '@adonisjs/core/http'
+
+import User from '#models/user'
+import userSchema from '../../utils/schemas.js'
+
+export default class UsersController {
+  async signup({ request, response }: HttpContext) {
+    try {
+      const data = request.only(['email', 'password'])
+
+      const { error } = userSchema.validate(data)
+
+      if (error) {
+        return response.status(422).json({ message: error.details[0].message })
+      }
+
+      // Verify if the email already exists in DB
+      const emailExists = await User.query().where('email', data.email).first()
+
+      if (emailExists) {
+        return response.status(400).json({ message: 'E-mail already in use.' })
+      }
+
+      const newUser = await User.create(data)
+
+      // const token = await Auth.generateToken(user)
+      const token = 'token fictício asdfasdfasdf'
+
+      return response.status(201).json({ token })
+    } catch (error) {
+      console.log(error)
+      return response.status(500).json({ message: 'Internal server error' })
+    }
+  }
+}
