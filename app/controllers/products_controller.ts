3401ac9df6d11719e6feecff9bdee73a3381e42a
Clinton Brito <clintoncsb@gmail.com>
2024-07-08 15:18:01 -0300

    ✨ feat: create `ProductsController.update` method

--- app/controllers/products_controller.ts
+++ app/controllers/products_controller.ts
@@ -20,8 +20,10 @@ export default class ProductsController {
         return response.status(422).json({ message: error.details[0].message })
       }
 
-      if (!payload.name || !payload.description || !payload.price) {
-        return response.status(422).json({ message: 'Name, price and quantity are required.' })
+      if (!payload.name || !payload.description || !payload.price || !payload.userId) {
+        return response
+          .status(422)
+          .json({ message: 'Name, description, price and userId are required.' })
       }
 
       const product = await Product.create({
@@ -33,6 +35,53 @@ export default class ProductsController {
 
       return response.status(201).json({ message: 'Product created successfully.' })
     } catch (error) {
+      console.log(error)
+      return response.status(500).json({
+        message: 'Internal server error',
+        error: error.message,
+      })
+    }
+  }
+
+  async update({ request, response }: HttpContext) {
+    try {
+      const token = request.header('Authorization')?.split(' ')[1]
+      const user = jwt.verify(token as string, process.env.APP_KEY || 'topsecret')
+
+      if (!user) {
+        return response.status(401).json({ message: 'Invalid token.' })
+      }
+
+      const { id } = request.params()
+      const payload = request.only(['name', 'description', 'price', 'userId'])
+      const { error } = productSchema.validate(payload)
+
+      if (error) {
+        return response.status(422).json({ message: error.details[0].message })
+      }
+
+      if (!payload.name || !payload.description || !payload.price || !payload.userId) {
+        return response
+          .status(422)
+          .json({ message: 'Name, description, price and userId are required.' })
+      }
+
+      const product = await Product.findOrFail(id)
+
+      product.merge({
+        name: payload.name,
+        description: payload.description,
+        price: payload.price,
+        userId: payload.userId,
+      })
+
+      await product.save()
+
+      return response.status(200).json({ message: 'Product updated successfully.' })
+    } catch (error) {
+      if (error.code === 'E_ROW_NOT_FOUND') {
+        return response.status(404).json({ message: 'Product not found.' })
+      }
       console.log(error)
       return response.status(500).json({
         message: 'Internal server error',
